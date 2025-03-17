a9ae25bd2eb15905130400c60ee5952ceb69b266
abrahamL1 <tdlazaro4247@gmail.com>
2025-03-17 11:19:13 -0700

    commit to github

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -6,49 +6,86 @@
 //
 
 import SwiftUI
+import AVFAudio
 
 struct ContentView: View {
     @State private var message = ""
-    @State private var imageString = ""
+    @State private var imageName = ""
+    @State private var lastMessageNumber = -1
+    @State private var lastImageNumber = -1
+    @State private var lastSoundNumber = -1
+    @State private var audioPlayer: AVAudioPlayer!
+    let numberOfImages = 10
+    let numberOfSounds = 6
+    
     var body: some View {
         VStack {
-            Spacer()
-            Image(systemName: imageString)
+            
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+                .multilineTextAlignment(.center)
+                .minimumScaleFactor(0.5)
+                .frame(height: 120)
+                .animation(.easeInOut(duration: 0.15), value: message)
+            
+            
+            Image(imageName)
                 .resizable()
                 .scaledToFit()
                 .imageScale(.large)
-                .foregroundStyle(.orange)
-                
-            Text(message)
-                .font(.largeTitle)
-                .fontWeight(.ultraLight)
+                .clipShape(RoundedRectangle(cornerRadius: 30))
+                .shadow(radius: 30)
+                .animation(.default, value: imageName)
             Spacer()
             
-            Button("Press me!") {
-               let message1 = "You are Awesome!"
-                let message2 = "You are Great!"
-                let imageString1 = "hand.thumbsup"
-                let imageString2 = "sun.max.fill"
-                
-//                if message == message1 {
-//                   message = message2
-//                    imageString = imageString2
-//                } else {
-//                    message = message1
-//                    imageString = imageString1
-//                }
-                message = ( message == message1 ? message2 : message1)
-                imageString = ( imageString == imageString1 ? imageString2 : imageString1)
-                
-            }
-            .buttonStyle(.borderedProminent)
-            .font(.title2)
-            .tint(.orange)
             
-            .padding()
+            Button("Show Message") {
+                
+                let messages = ["You Are Awesome!","When the Genius Bar Needs Help,They Call You!","My Favorite Planet in the Universe is Planet Earth!", "You Are Great!", "You Are Fantastic!", "Fabulous? That's You!", "You Make Me Smile!"]
+                
+                
+                
+                lastMessageNumber = nonRepeatingRandom(lastNumber: lastMessageNumber, upperBound: messages.count-1)
+                message = messages[lastMessageNumber]
+
+                lastImageNumber = nonRepeatingRandom(lastNumber: lastImageNumber, upperBound: numberOfImages-1)
+                imageName = "image\(lastMessageNumber)"
+                
+                lastSoundNumber = nonRepeatingRandom(lastNumber: lastSoundNumber, upperBound: numberOfSounds-1)
+                playSound(soundName: "sound\(lastSoundNumber)")
+            }
+            
+        }
+        .buttonStyle(.borderedProminent)
+        .font(.title2)
+        .tint(.blue)
+        
+        .padding()
+    }
+    func nonRepeatingRandom(lastNumber: Int, upperBound: Int) -> Int {
+        var newNumber: Int
+        repeat {
+            newNumber = Int.random(in: 0...upperBound)
+        } while newNumber == lastNumber
+        return newNumber
+    }
+    
+    func playSound(soundName: String) {
+        guard let soundFile = NSDataAsset(name: soundName) else {
+            print("😡Could not read file named \(soundName)")
+            return
+        }
+        do {
+            audioPlayer = try AVAudioPlayer(data: soundFile.data)
+            audioPlayer.play()
+        } catch {
+            print("😡 ERROR: \(error.localizedDescription) creating audioPlayer")
         }
     }
 }
+
 
 #Preview {
     ContentView()
